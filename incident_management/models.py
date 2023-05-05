d43418cd6ca573857526d3862445684df0640b1c
amaan.ansari <amaan.ansari@vfirst.com>
2023-05-06 02:07:49 +0530

    incident_id randomized and removed from fe

--- incident_management/models.py
+++ incident_management/models.py
@@ -1,5 +1,8 @@
+from datetime import datetime
+
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.crypto import get_random_string
 
 
 class Incident(models.Model):
@@ -24,3 +27,19 @@ class Incident(models.Model):
 
     def __str__(self):
         return self.incident_id
+
+    def save(self, *args, **kwargs):
+        # Generate a random 5-digit number
+        random_number = get_random_string(length=5, allowed_chars='1234567890')
+
+        # Get the current year
+        current_year = datetime.now().year
+
+        # Construct the incident ID in the required format
+        incident_id = f'RMG{random_number}{current_year}'
+
+        # Set the incident ID if it's blank
+        if not self.incident_id:
+            self.incident_id = incident_id
+
+        super().save(*args, **kwargs)
