71650b607454ea5bf5997284fee2954bbd32a451
amaan.ansari <amaan.ansari@vfirst.com>
2023-07-18 18:49:29 +0530

    form filling and push

--- mysite/my_portfolio/serializers.py
+++ mysite/my_portfolio/serializers.py
@@ -6,3 +6,6 @@ class ContactMeSerializer(serializers.ModelSerializer):
     class Meta:
         model = ContactMe
         fields = '__all__'
+
+    # def to_internal_value(self, data):
+    #     return super().to_internal_value(data)
