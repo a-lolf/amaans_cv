5549a5ee794f781e65789da662b2f5b98129eb50
Amaan <ansari.amaan@outlook.com>
2023-07-19 18:34:46 +0530

    set environ vars

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yxix0ie@kz$o=2#febhsgm#%ygd#dvz$_robuu+ws6xh0xj*+('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('PORTFOLIO_DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['amaanansari.pythonanywhere.com', 'amaanans.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['amaanansari.pythonanywhere.com', 'amaanans.pythonanywhere.com']
+if DEBUG:
+    ALLOWED_HOSTS += ['127.0.0.1']
 
 # Application definition
 
@@ -76,8 +78,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('PORTFOLIO_DBNAME', 'my_portfolio'),
+        'USER': os.environ.get('PORTFOLIO_DBUSER', 'root'),
+        'PASSWORD': os.environ.get('PORTFOLIO_DBPSK', 'root'),
+        'HOST': os.environ.get('PORTFOLIO_DBHOST', '127.0.0.1'),
     }
 }
 
